74b584ec0ad2b4999908d2dd08ad75145a51a974
donasunny5 <donasunny5@gmail.com>
2023-08-02 17:16:08 -0400

    Created First Screen UI

---
+++ iOS Project 2/NetworkLayer.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkLayer.swift
+//  iOS Project 2
+//
+//  Created by Mac on 2023-08-02.
+//
+
+import Foundation
+
+class NetworkLayer {
+    
+    private init () { }
+    
+    static let shared = NetworkLayer()
+    
+    func getCityWeatherFor(cityName: String, completion: @escaping (WeatherModel?, Error?) -> ()) {
+        let url = "http://api.weatherapi.com/v1/current.json?key=481c87a2c9c54e2e992210038230208&q=\(cityName)&aqi=no".addingPercentEncoding(withAllowedCharacters:
+                .urlFragmentAllowed)!
+        guard let finalUrl = URL(string: url) else { return }
+        let task = URLSession.shared.dataTask(with: finalUrl, completionHandler: { data, response, error in
+            guard let jsonData = data else { return }
+            do {
+                let weatherResponse = try JSONDecoder().decode(WeatherModel.self, from: jsonData)
+                completion(weatherResponse, nil)
+            } catch let parseErr {
+                print("JSON Parsing Error", parseErr)
+                completion(nil, parseErr)
+            }
+        })
+        task.resume()
+        
+    }
+}
--- iOS Project 2/ViewController.swift
+++ iOS Project 2/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var tempratureLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var WeatherStatusLabel: UILabel!
+    @IBOutlet weak var citiesButton: UIButton!
+    @IBOutlet weak var SearchButton: UIButton!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var locationButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func segmentControlChanged(_ sender: Any) {
+    }
+    
+    @IBAction func locationButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func citiesButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func searchButtonTapped(_ sender: Any) {
+    }
+    
 }
 
